bc5b9aff21019dc2c2d0222092916824dad905c9
Yoel Vaizman <yoelvaizman@gmail.com>
2020-06-25 18:03:22 +0300

    adding leave program option

--- helloWorld.cs
+++ helloWorld.cs
@@ -4,12 +4,22 @@ namespace HelloWorld
 {
   class Program
   {
+    
     static void Main(string[] args)
     {
       Console.WriteLine("Hello World!");
       Console.WriteLine("Enter something please:");
       string result = Console.ReadLine();
       Console.WriteLine("You entered: {0}",result);
+      Console.WriteLine("Do you want To leave the program (yes): ");
+      result = Console.ReadLine();
+      while (result != "yes")
+        {
+            Console.WriteLine("Invalid input! ");
+            Console.WriteLine("Do you want To leave the program (yes): ");
+            result = Console.ReadLine();
+        }
+     
 
     }
   }
